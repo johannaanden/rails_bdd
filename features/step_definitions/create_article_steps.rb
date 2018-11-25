6df52e5cd3f8a4f2bc0518b1f03b596854408394
Johanna Andén <johannaanden@Johannas-MBP.lan>
2018-11-25 09:49:53 +0100

    Removes unnecessarily test

--- features/step_definitions/create_article_steps.rb
+++ features/step_definitions/create_article_steps.rb
@@ -14,10 +14,6 @@ When("I fill in {string} with {string}") do |field, value|
     fill_in field, with: value
 end
   
-When("I click {string} button") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
-end
-  
 Then("I should be on {string} page") do |string|
     pending # Write code here that turns the phrase above into concrete actions
 end
