125bd9090d2faea40d4ae1b49470a3bd86ab990a
Johanna Andén <johannaanden@Johannas-MBP.lan>
2018-11-25 15:10:52 +0100

    Adds action to create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,10 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    @article = Article.new(article_params)
 
+    @article.save
+    redirect_to @article
   end
 
   def show
