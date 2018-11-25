b237b0aa03a4e2df1aa1309c94248c7673673093
Johanna Andén <johannaanden@Johannas-MBP.lan>
2018-11-25 15:12:37 +0100

    Creates action article_params

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,4 +17,10 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  private
+    def article_params
+        params.require(:article).permit(:title, :content)
+    end
+
 end
